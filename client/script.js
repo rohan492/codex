d056ba469436ef814d07ee49f16572634e65f5b7
rohan mathur <rohanmathur@rohans-MacBook-Air.local>
2022-12-24 10:45:11 +0530

    changed the client/script.js to fetch response from server deployed on the web

--- client/script.js
+++ client/script.js
@@ -86,8 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    // const response = await fetch('https://codex-im0y.onrender.com/', {
-    const response = await fetch('http://localhost:5001', {
+    const response = await fetch('https://codex-9pm2.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
